aa1f0c86370728cdd38445798fb6bd47ab4e0124
Alberto Mijares <almilo@almilo.es>
2016-07-26 17:59:37 +0200

    (refactor): substitute home-brewed static server with express

--- graph/index.js
+++ graph/index.js
@@ -1,33 +1,29 @@
-var http = require('http');
-var url = require('url');
 var path = require('path');
 var fs = require('fs');
+var express = require('express');
 var livereload = require('livereload');
 var openUrl = require('../lib').openUrl;
 
 module.exports = function (schemaTextFileName, port, openInBrowser) {
     port = port || 4000;
+    schemaTextFileName = path.resolve(schemaTextFileName);
 
     var schemaTextFileNameExtension = path.extname(schemaTextFileName).slice(1);
     var livereloadConfiguration = {exts: [schemaTextFileNameExtension]};
 
-    http.createServer(staticContentHandler).listen(port, onServerReady);
+    express()
+        .use(express.static(__dirname))
+        .get('/schema', schemaHandler)
+        .listen(port, onServerReady);
     livereload.createServer(livereloadConfiguration).watch(schemaTextFileName);
 
-    function staticContentHandler(req, res) {
-        var requestPath = getPath(req);
-        var filePath = requestPath === '/schema' ?
-            path.resolve(schemaTextFileName) :
-            path.join(__dirname, requestPath.replace('/', path.sep));
-
-        if (fs.existsSync(filePath)) {
-            res.writeHead(200, {'Content-Type': getMimeType(requestPath)});
-
-            return res.end(fs.readFileSync(filePath));
+    function schemaHandler(req, res) {
+        if (fs.existsSync(schemaTextFileName)) {
+            res.send(fs.readFileSync(schemaTextFileName));
         } else {
-            res.writeHead(404, 'Not found.');
+            console.error('Schema file: "' + schemaTextFileName + '" not found.');
 
-            return res.end();
+            res.status(404).send('Not found.');
         }
     }
 
@@ -38,26 +34,10 @@ module.exports = function (schemaTextFileName, port, openInBrowser) {
         }
 
         console.log('Graph server listening on port:', port);
-        console.log('Opening graph in browser...');
 
         if (openInBrowser) {
+            console.log('Opening graph in browser...');
             openUrl(' http://localhost:' + port);
         }
     }
 };
-
-function getPath(req) {
-    var path = url.parse(req.url).path;
-
-    return path !== '/' ? path : '/index.html';
-}
-
-function getMimeType(fileName) {
-    var mimeTypes = {
-        'html': 'text/html',
-        'css': 'text/css'
-    };
-    var extension = path.extname(fileName).slice(1).toLowerCase();
-
-    return mimeTypes[extension] || 'text/plain';
-}
